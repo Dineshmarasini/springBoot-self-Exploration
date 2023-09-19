57c9a16d65b02776aca4be5dfc100f16fec92e55
Dineshmarasini <99908688+Dineshmarasini@users.noreply.github.com>
2023-09-20 00:38:50 +0545

    Rest API with postMan

---
+++ BootWithRESTAPI/src/main/java/com/api/book/service/BookService.java
@@ -0,0 +1,33 @@
+package com.api.book.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.api.book.entities.Book;
+@Component
+public class BookService {
+	private static List<Book> list=new ArrayList<>();
+	
+	static {
+		list.add(new Book(1,"summar Love", "subin Bhattarai"));
+		list.add(new Book(2,"Priya Sufi", "subin Bhattarai"));
+		list.add(new Book(3,"Palpasa cafe","Narayan Wagle"));
+		
+	}
+	
+	public List<Book> getAllBooks(){
+		return list;
+	}
+	
+	public Book getBookById(int id) {
+		Book book=null;
+		
+		 
+	book=list.stream().filter(e->e.getId()==id).findFirst().get();
+	return book;
+		
+	}
+
+}
